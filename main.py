a96676876b71fc3850546f420669edc98f171195
Emenem <vovamihno979@gmail.com>
2026-04-26 19:20:51 +0300

    back

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from pygame import *
+window = display.set_mode((700,500))
+display.set_caption('Run')
+background = transform.scale(image.load('i.webp'), (700,500))
+game = True
+while game:
+    window.blit(background,(0,0))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    display.update()
